3ed626382d1d77f4df21fcdfb44e0fe4fe5d5702
Andrei Dzebysh <presnia@yahoo.com>
2020-12-24 22:15:27 +0300

    Realized functionality of audio player

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,5 @@
+import addZero from './supScript.js';
+
 export const videoPlayerInit = () => {
   const videoPlayer = document.querySelector('.video-player');
   const videoButtonPlay = document.querySelector('.video-button__play');
@@ -23,8 +25,6 @@ export const videoPlayerInit = () => {
       videoPlayer.pause();
     }
   };
-
-  const addZero = n => n < 10 ? '0' + n : n;  
 
   const stopPlay = () => {
     videoPlayer.pause();
